3fa6ef5cc4ddef9ba4c217ffa2bd1188ed4550ba
Viveka00 <162979218+Viveka00@users.noreply.github.com>
2024-08-21 09:47:42 +0530

    Add files via upload

---
+++ major/digital.js
@@ -0,0 +1,4 @@
+let btn_1=document.getElementById("btn_1");
+btn_1.onclick = function(){
+    window.location.href = "digital.html";
+}
---
+++ major/script.js
@@ -0,0 +1,21 @@
+let btn_study = document.getElementById("btn-study");
+let btn_farming = document.getElementById("btn-farming");
+let btn_job = document.getElementById("btn-job");
+let btn_1=document.getElementById("btn_1");
+
+btn_study.onclick = function(){
+    window.location.href = "Academics.html";
+}
+
+
+btn_farming.onclick = function(){
+    window.location.href = "farmer.html";
+}
+
+
+btn_job.onclick = function(){
+    window.location.href = "job.html";
+}
+btn_1.onclick = function(){
+    window.location.href = "digital.html";
+}
